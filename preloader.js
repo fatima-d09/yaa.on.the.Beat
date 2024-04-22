3f26769925a3b6968564ba3bd1fee53eba6cb6fc
Fatima Dembele <97924915+fatima-d09@users.noreply.github.com>
2024-04-22 16:31:48 -0400

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,4 @@
+ // Set a timeout to hide the preloader after 5 seconds
+ setTimeout(function() {
+  document.getElementById('preloader').style.display = 'none';
+}, 3000); // 5000 milliseconds equals 5 seconds
---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     // Get the hamburger element
+//     var hamburger = document.querySelector('.hamburger');
+//     // Get the nav menu element
+//     var navBar = document.getElementById('navbar');
+  
+//     // Add a click event listener to the hamburger button
+//     hamburger.addEventListener('click', function() {
+//       // Toggle the nav menu's visibility
+//       navBar.classList.toggle('nav-show');
+//     });
+//   });
+  
